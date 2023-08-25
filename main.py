9245b85ba5ea7fccd09cd90b04193c34ddd9a940
Turgay <turgaj.abdullajev@gmail.com>
2023-08-25 23:43:54 +0400

    add login, logout, register routes

--- main.py
+++ main.py
@@ -8,5 +8,20 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/register')
+def register():
+    pass
+
+
+@app.route('/login')
+def login():
+    pass
+
+
+@app.route('/logout')
+def logout():
+    pass
+
+
 if __name__ == "__main__":
     app.run(debug=True)
