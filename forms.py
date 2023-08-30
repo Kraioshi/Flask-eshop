78c57976ca8e7ab31ccab0f11fa4777ca483410c
Turgay <turgaj.abdullajev@gmail.com>
2023-08-30 08:14:59 +0400

    changed product description field from StringField to TextAreaField in AddProductForm

--- forms.py
+++ forms.py
@@ -1,13 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, FloatField, BooleanField, IntegerField
+from wtforms import StringField, SubmitField, PasswordField, FloatField, BooleanField, IntegerField, TextAreaField
 from wtforms.validators import DataRequired, URL
 
 
 class AddProductForm(FlaskForm):
     title = StringField("Product Name", validators=[DataRequired()])
     subtitle = StringField("Short Description", validators=[DataRequired()])
-    description = StringField("Product Description", validators=[DataRequired()])
+    description = TextAreaField("Product Description",
+                                render_kw={"style": "height: 120px;"},
+                                validators=[DataRequired()])
     price = FloatField("Price", validators=[DataRequired()])
     quantity = IntegerField("Quantity", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    img_url = StringField("Product Image URL", validators=[DataRequired(), URL()])
     submit = SubmitField("Add Product")
