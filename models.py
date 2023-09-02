420e90b58eee519ec4782f92f008206859261a22
Turgay <turgaj.abdullajev@gmail.com>
2023-09-02 22:34:47 +0400

    Changed id in every model in models.py by adding unsigned=True and autoincrement=True

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from sqlalchemy.orm import relationship
-from decimal import Decimal
+from sqlalchemy.dialects.mysql import INTEGER
 
 db = SQLAlchemy()
 
 wishlist_table = db.Table(
     'wishlist_table',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True, ),
-    db.Column('product_id', db.Integer, db.ForeignKey('product.id'), primary_key=True)
+    db.Column('user_id', INTEGER(unsigned=True), db.ForeignKey('user.id'), primary_key=True),
+    db.Column('product_id', INTEGER(unsigned=True), db.ForeignKey('product.id'), primary_key=True)
 )
 
 cart_table = db.Table(
@@ -20,18 +20,17 @@ cart_table = db.Table(
 
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-
     wishlist = relationship('Product', secondary=wishlist_table, back_populates='wishlist_user')
     cart = relationship('Product', secondary=cart_table, back_populates='cart_user')
 
 
 class Product(db.Model):
     __tablename__ = 'product'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
     title = db.Column(db.String(250), nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
     description = db.Column(db.String, nullable=False)
