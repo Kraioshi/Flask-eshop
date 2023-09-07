b3535fdb5129a547889e886958aa41dc6008dc1a
Turgay <turgaj.abdullajev@gmail.com>
2023-09-07 23:51:00 +0400

    Added stripe payment (incorrect price)

--- app/blueprints/payment/checkout.py
+++ app/blueprints/payment/checkout.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 import stripe
 
-public_key =
-stripe.api_key =
+public_key = 'pk_test_51NnnjGCKKdvgdbMlhv3RFUkMBwSKj1wArUrXhIPBEaLMH6UHwYlKePmipB4plarS5A7pYjTo24DKWTUXutEQdXNo00jAdYi28v'
+stripe.api_key = 'sk_test_51NnnjGCKKdvgdbMlVHrzh80PDBQY9Fg4FS815OlTzUAKclhDktv7XNaGoBE0tZWetX55tEfFrAnmxPdGEFThjv9M00BdMyO9Lc'
 
 
 checkout_bp = Blueprint('checkout', __name__)
@@ -12,6 +12,7 @@ checkout_bp = Blueprint('checkout', __name__)
 @checkout_bp.route('/checkout', methods=["GET"])
 @login_required
 def checkout_get():
-    return render_template('payment/checkout.html', public_key=public_key)
+    user_cart = current_user.cart
+    return render_template('payment/checkout.html', public_key=public_key, cart_products=user_cart)
 
 
--- app/blueprints/payment/payment.py
+++ app/blueprints/payment/payment.py
@@ -1,15 +1,17 @@
 from flask import Blueprint, request, url_for, redirect
+from flask_login import current_user
 import stripe
 from stripe import Customer, Charge
 
-public_key =
-stripe.api_key =
+public_key = 'pk_test_51NnnjGCKKdvgdbMlhv3RFUkMBwSKj1wArUrXhIPBEaLMH6UHwYlKePmipB4plarS5A7pYjTo24DKWTUXutEQdXNo00jAdYi28v'
+stripe.api_key = 'sk_test_51NnnjGCKKdvgdbMlVHrzh80PDBQY9Fg4FS815OlTzUAKclhDktv7XNaGoBE0tZWetX55tEfFrAnmxPdGEFThjv9M00BdMyO9Lc'
 
 payment_bp = Blueprint('payment', __name__)
 
 
 @payment_bp.route('/payment', methods=["POST"])
 def payment():
+    user_cart = current_user.cart
     # CUSTOMER INFORMATION
     customer = Customer.create(email=request.form['stripeEmail'],
                                source=request.form['stripeToken'])
@@ -17,8 +19,8 @@ def payment():
     #PAYMENT
     charge = Charge.create(
         customer=customer.id,
-        amount=1999,
-        current='usd',
+        amount=user_cart.price,
+        currency='usd',
         description='The Description Here',
 
     )
